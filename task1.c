e430ec7c1cd6ad15f43635ee262e14d08825d1d6
Ian Bricker <brickerian@gmail.com>
2019-05-02 04:47:26 +0000

    task1

---
+++ task1.c
@@ -0,0 +1,19 @@
+//Write a C function which takes a single int * arg and zeroes it
+/*#include <stdio.h>
+
+void zeroInt(int *x);
+
+int main() 
+{
+    int *x, k = 3;
+    x = &k;
+    printf("k: %d\n", k);
+    zeroInt(&x);
+    printf("return: %d", x);
+    
+}
+
+void zeroInt(int *x) 
+{
+    *x = 0;
+} */
