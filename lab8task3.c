ff5e737587652e433d46148ec93f9875b6dc47cc
Ian Bricker <brickerian@gmail.com>
2019-05-08 03:23:17 +0000

    done task 3

--- lab8task3.c
+++ lab8task3.c
@@ -1,22 +1,23 @@
 //C function that accepts 3 int * args, assignes the mean of the three numbers to the first argument and zeros the others.
 #include <stdio.h>
+#include <math.h>
 
 void mean(int *a, int *b, int *c);
 
 int main() 
 {
-    int x = 5, y = 6, z = 7;
+    int x = 1, y = 2, z = 3;
     printf("data: x = %d, y = %d, z = %d\n", x, y, z);
-    mean(&x, &y, &x);
+    mean(&x, &y, &z);
     printf("mean: %d\nzeros: %d %d\n", x, y, z);
+    
+    return 0;
     
 }
 
 void mean(int *a, int *b, int *c)
 {
-    int mean;
-    mean = (*a + *b + *c)/3;
-    a = &mean;
+    *a = (int)round((*a + *b + *c)/3.0);
     *b = 0;
     *c = 0;
 }
